c3d50b434bfb2489d1fa2f75648beff5a0a6187a
Olga Kovalenko <olha.smitholha@gmail.com>
2022-06-09 12:31:26 +0300

    Form menu from item list

--- src/App.js
+++ src/App.js
@@ -9,20 +9,23 @@ function httpGet(theUrl) {
 
 function App() {
     const menuItems = JSON.parse(httpGet('https://edc699c3-6045-4324-9406-69e6cfeb1671.mock.pstmn.io/menu'));
-    const firstItem = menuItems[0];
 
     return (
         <main id="main">
-            <MenuItem image={firstItem.image}
-                        name={firstItem.name}
-                        price={firstItem.price/100}
-                        weight={firstItem.weight}
-                        ingredients={firstItem.ingridients}
-                        isAvailable={firstItem.isAvailable}
-                        id={firstItem.id}
-                        category={firstItem.category}
-            />
-
+            {
+                menuItems.map((item) =>
+                    <MenuItem key={item.id}
+                              image={item.image}
+                              name={item.name}
+                              price={item.price/100}
+                              weight={item.weight}
+                              ingredients={item.ingredients}
+                              isAvailable={item.isAvailable}
+                              id={item.id}
+                              category={item.category}
+                    />
+                )
+            }
         </main>
     );
 }
